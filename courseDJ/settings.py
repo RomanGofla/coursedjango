bb9a3d58c2773cdc3357515d7fbdc3ecaabbcafa
Gofla <gofla95@list.ru>
2020-02-09 21:54:44 +0300

    Third lesson finished

--- courseDJ/settings.py
+++ courseDJ/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mptt',
     'blog',
 ]
 
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'courseDJ.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
